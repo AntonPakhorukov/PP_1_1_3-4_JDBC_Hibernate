1e2757465123dd4fd59616d9e04301fa38d93346
Anton <pakhorukov.a@mail.ru>
2024-07-22 20:57:15 +0700

    MySQL_JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.dropUsersTable();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Garry", "Potter" , (byte) 14);
+        userDao.saveUser("Ron", "Uisley" , (byte) 14);
+        userDao.saveUser("Germiona", "Granger" , (byte) 15);
+        userDao.saveUser("Severus", "Snape" , (byte) 45);
+        System.out.println();
+        userDao.getAllUsers();
+//        userDao.removeUserById(2);
+//        userDao.getAllUsers();
+//        userDao.cleanUsersTable();
+//        userDao.getAllUsers();
+//        userDao.dropUsersTable();
     }
 }
